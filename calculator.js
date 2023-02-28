9f106999964dbdb91ab2f3edccb25c3aab8bd7d6
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-02-28 11:30:56 -0600

    stuck on how to start jest testing for calculator function

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 const add = function() {
-	
+
 };
 
 const subtract = function() {
@@ -21,8 +21,11 @@ const power = function() {
 const factorial = function() {
 	
 };
+console.log(add(2, 7));
+numbers = [0,1,2,3,4,5,6,7,8,9];
 
 // Do not edit below this line
+/*
 module.exports = {
   add,
   subtract,
@@ -31,4 +34,4 @@ module.exports = {
   power,
   factorial
 };
-
+*/
